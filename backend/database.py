5450c7c3eed715162041741dad923d71ce0d6ff5
Pham Huy Canh <uazalo97@gmail.com>
2026-06-28 19:50:08 +0700

    fix: db prod

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from sqlalchemy.engine import URL
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in the environment variables.")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST", "db")
+    db_name = os.getenv("DB_NAME")
+    
+    if db_user and db_password and db_host and db_name:
+        DATABASE_URL = URL.create(
+            drivername="postgresql",
+            username=db_user,
+            password=db_password,
+            host=db_host,
+            port=5432,
+            database=db_name
+        )
+    else:
+        raise ValueError("DATABASE_URL is not set in the environment variables.")
 
 engine = create_engine(DATABASE_URL)
 
